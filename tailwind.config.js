8363c3c58db94b910e5b91812541f9fafd6cbff9
luis <83016329+ruuiisu@users.noreply.github.com>
2021-12-16 16:36:46 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+    fontSize: {
+      'xs': '0.20rem',
+      'sm': '0.22rem',
+      'base': '0.25rem',
+    },
+  },
+  plugins: [],
+}
