6601ce6f66c44bb3e569b3c1be1ad4c2bf53360a
Luis Gjokaj <ruuiisu>
2021-12-23 15:01:37 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+    fontSize: {
+      'xs': '0.3rem',
+      'sm': '0.4rem',
+      'base': '0.25rem',
+      'md': '0.75rem',
+    },
+  },
+  plugins: [],
+}
