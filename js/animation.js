66f44846e62b3b988a3a11641cfaa92b1e167170
Luis Gjokaj <ruuiisu>
2022-01-13 08:21:39 +0100

    Some commit man

--- js/animation.js
+++ js/animation.js
@@ -1,13 +1,15 @@
-document.getElementById("animation-main").addEventListener("mouseover", mouseOver);
-document.getElementById("animation-main").addEventListener("mouseout", mouseOut);
+document.getElementById("animation-main").addEventListener("click", mouseOver);
+document.getElementById("animation-pic").style.transition = "all 1.2s";
+document.getElementById("animation-text").style.transition = "all 1.2s";
+let count = 0;
 
 function mouseOver() {
-    document.getElementById("animation-pic").style.marginRight = "1rem";
-    document.getElementById("animation-text").style.marginLeft = "1rem";
+    document.getElementById("animation-pic").style.marginLeft = "5rem";
+    document.getElementById("animation-text").style.marginRight = "7rem";
+    if (count === 1) {
+        document.getElementById("animation-pic").style.marginLeft = "0rem";
+        document.getElementById("animation-text").style.marginRight = "0rem";
+        count = -1;
+    }
+    count += 1;
 }
-
-function mouseOut() {
-    document.getElementById("animation-pic").style.marginRight = "0rem";
-    document.getElementById("animation-text").style.marginLeft = "0rem";
-}
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,25 @@ module.exports = {
   theme: {
     extend: {},
     fontSize: {
+      'xxs': '0.2rem',
       'xs': '0.3rem',
       'sm': '0.4rem',
       'base': '0.25rem',
       'md': '0.75rem',
+    },
+    colors: {
+      'real-dark': '#17141d',
+      'card-dark': '#17141a',
+      'white': '#ffffff',
+      'gray': '#aaaaaa',
+      'dark': '#27272a',
+      'black': '#000000',
+      'blue': '#0150ab',
+      'light-blue': '#3483de',
+    },
+    boxShadow: {
+      '3xl': '-0.3rem 0 0.9rem #000',
+      '2xl': '0 25px 50px -12px rgb(0 0 0 / 0.25)',
     },
   },
   plugins: [],
